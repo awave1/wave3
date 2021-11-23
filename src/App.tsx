fc1926b0f9ff28b22e951d2d5feeb419d7447992
awave1 <artgolovin96@gmail.com>
2021-11-23 15:03:02 -0700

    feat: connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 export function App() {
+  const [active, setActive] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  const checkWalletConnection = () => {
+    if (window.ethereum) {
+      console.log(window.ethereum);
+
+      setActive(true);
+    } else {
+      console.warn("Metamask isn't connected");
+    }
+  };
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
+
+  const onConnectWallet = async () => {
+    if (!active) {
+      console.warn("Metamask isn't connected");
+      return;
+    }
+
+    if (currentUser) {
+      return;
+    }
+
+    const { ethereum } = window;
+
+    const accounts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    console.log(accounts);
+    setCurrentUser(accounts[0]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>👋</p>
+        <p>{currentUser ? `👋` : `😱`}</p>
+        {active && !currentUser ? (
+          <button onClick={onConnectWallet}>Connect</button>
+        ) : null}
       </header>
     </div>
   );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+import type { EthereumProvider } from "hardhat/types";
