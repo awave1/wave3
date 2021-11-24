b5c87999fa6f07644f4d8d473e42505af33f818a
awave1 <artgolovin96@gmail.com>
2021-11-24 12:26:53 -0700

    feat: migrate default styles to vanilla

--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,11 +1,12 @@
 import { useApp } from "features/App/useApp";
+import { AppStyle, AppHeaderStyle } from "features/App/style.css";
 
 export function App() {
   const { models, handlers } = useApp();
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={AppStyle}>
+      <header className={AppHeaderStyle}>
         <p>{models?.user ? `👋` : `😱`}</p>
         {models.active && !models?.user ? (
           <button onClick={handlers.onConnectWalletClicked}>Connect</button>
