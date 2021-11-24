b80b417277c2016634b99c91fa5bff5675aef610
awave1 <artgolovin96@gmail.com>
2021-11-24 12:44:51 -0700

    fix: improve type for ethereum provider

--- src/domains/wallet/hooks/useWallet.ts
+++ src/domains/wallet/hooks/useWallet.ts
@@ -1,6 +1,10 @@
 import detectEthereumProvider from "@metamask/detect-provider";
+import { EthereumProvider } from "hardhat/types";
 import { useQuery } from "react-query";
 
 export function useWallet() {
-  return useQuery("wallet", () => detectEthereumProvider());
+  return useQuery<EthereumProvider>("wallet", async () => {
+    const provider = await detectEthereumProvider();
+    return provider as EthereumProvider;
+  });
 }
